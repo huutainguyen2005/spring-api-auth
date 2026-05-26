d47302358b839cb90fcf766e5a9bf897764db2e5
huutainguyen2005 <tainh.ce190284@gmail.com>
2026-05-26 11:38:02 +0700

    feat: add OAuth2 resource server configuration

--- resource-server/src/main/java/vn/edu/fpt/sba/configuration/SecurityConfig.java
+++ resource-server/src/main/java/vn/edu/fpt/sba/configuration/SecurityConfig.java
@@ -15,23 +15,50 @@ import org.springframework.security.web.SecurityFilterChain;
 @EnableWebSecurity
 public class SecurityConfig {
 
+//    @Bean
+//    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+//        http
+//                .authorizeHttpRequests(auth -> auth
+//                        .requestMatchers("/api/auth/**").permitAll()
+//                        .anyRequest().authenticated())
+////                .httpBasic(Customizer.withDefaults()) // Bật HTTP Basic Auth
+//                .formLogin(AbstractHttpConfigurer::disable) // Ẩn form login
+//                .csrf(AbstractHttpConfigurer::disable) // Tắt CSRF -> chỉ để dev
+//                .sessionManagement(s ->
+//                        s.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+//        ;
+//        return http.build();
+//    }
+//
+//    @Bean
+//    public AuthenticationManager authenticationManager(AuthenticationConfiguration config) throws Exception {
+//        return config.getAuthenticationManager();
+//    }
+
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-        http
-                .authorizeHttpRequests(auth -> auth
-                        .requestMatchers("/api/auth/**").permitAll()
-                        .anyRequest().authenticated())
-//                .httpBasic(Customizer.withDefaults()) // Bật HTTP Basic Auth
-                .formLogin(AbstractHttpConfigurer::disable) // Ẩn form login
-                .csrf(AbstractHttpConfigurer::disable) // Tắt CSRF -> chỉ để dev
-                .sessionManagement(s ->
-                        s.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
-        ;
+        http.securityMatcher("/api/v1/artists/**")
+                .authorizeHttpRequests(authorize -> authorize
+                        .anyRequest().hasAuthority("SCOPE_artists.read"))
+                .oauth2ResourceServer(oauth2 -> oauth2
+                        .jwt(Customizer.withDefaults()));
+
         return http.build();
     }
 
+    // Tất cả request match chỉ được câp phếp nếu JWT Authority: SCOPE_openid | SCOPE_profile | SCOPE_email
+    // VD: Token (jwt) có scope profile -> Spring hiểu authority là SCOPE_profile, v.v
+    // VD: Token (jwt) có scope artists.read -> Spring hiểu authority là SCOPE_artists.read, v.v
+    // .oauth2ResourceServer() -> báo hiệu cho biết đây là 1 resource server
+    // và dùng JWT để xác thực, dùng các cấu hình mặc định
+    // Resource server sẽ validate:
+    // - đọc token
+    // - signature
+    // - expires
+    // - scope
+
     @Bean
-    public AuthenticationManager authenticationManager(AuthenticationConfiguration config) throws Exception {
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration config) throws Exception{
         return config.getAuthenticationManager();
     }
 }
