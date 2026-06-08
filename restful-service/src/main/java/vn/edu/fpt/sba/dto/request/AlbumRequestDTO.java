81a579f26c776576559bfc3a9114373da14060c9
Tai Nguyen <tainh.ce190284@gmail.com>
2026-06-08 11:21:02 +0700

    feat: change validation for Album

--- restful-service/src/main/java/vn/edu/fpt/sba/dto/request/AlbumRequestDTO.java
+++ restful-service/src/main/java/vn/edu/fpt/sba/dto/request/AlbumRequestDTO.java
@@ -6,7 +6,7 @@ import jakarta.validation.constraints.Size;
 
 public record AlbumRequestDTO(
         @NotBlank(message = "Title is required")
-        @Size(min = 1, max = 255, message = "Title must be between 1 and 255 characters")
+        @Size(min = 5, max = 255, message = "Title must be between 1 and 255 characters")
         String title,
 
         @NotNull(message = "Artist ID is required")
